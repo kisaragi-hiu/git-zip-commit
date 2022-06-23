f9c4e194eddf4a73c131dab896b63edbb0659e24
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-06-24 05:58:34 +0900

    Finish writing tests

--- tests/test-git-zip-commit.el
+++ tests/test-git-zip-commit.el
@@ -24,8 +24,11 @@
   (it "returns list of changed files"
     ;; We need a second commit
     (expect (git-zip-commit--commit-changed-files
-             "23d4d974d6d47aa42842b377de1e046d6a772d4d")
-            :to-equal "Initial commit"))
+             "440109d210efdfa63e72793f47df8804675bb26d")
+            :to-have-same-items-as '("tests/test-git-zip-commit.el")))
   (it "returns nil if it fails"
-    (expect (git-zip-commit--commit-message "")
+    (expect (git-zip-commit--commit-changed-files "")
+            :to-eq nil)
+    (expect (git-zip-commit--commit-changed-files
+             "23d4d974d6d47aa42842b377de1e046d6a772d4d")
             :to-eq nil)))
